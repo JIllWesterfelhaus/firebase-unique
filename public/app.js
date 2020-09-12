6a7824e3d380af8d17a23fdf1ee4a8cd145de8d7
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-09-12 15:41:29 -0500

    edited app.js

--- public/app.js
+++ public/app.js
@@ -4,12 +4,12 @@ const whenSignedIn = document.getElementById('whenSignedIn');
 const whenSignedOut = document.getElementById('whenSignedOut');
 
 const signInBtn = document.getElementById('signInBtn');
-const signOutBtn = document.getElementById('signOutBtn')
+const signOutBtn = document.getElementById('signOutBtn');
 
-const userDetails = documents.getElementById('userDetails');
+const userDetails = document.getElementById('userDetails');
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
-signInBtn.onclick = () => auth.siginInWithPopup(provider);
+signInBtn.onclick = () => auth.signInWithPopup(provider);
 
 signOutBtn.onclick = () => auth.signOut();
