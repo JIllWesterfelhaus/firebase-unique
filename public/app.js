d52cf66ab00412fef2b64902f1fba25136041137
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-09-14 16:51:16 -0500

    updated app.js, sign in and out

--- public/app.js
+++ public/app.js
@@ -14,4 +14,17 @@ signInBtn.onclick = () => auth.signInWithPopup(provider);
 
 signOutBtn.onclick = () => auth.signOut();
 
-auth.onAuthStatedChanged();
+auth.onAuthStatedChanged(user => {
+    if (user) {
+        //signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        //not signed in
+        whenSignedIn.hidden = true;
+        whenSignedIn.hidden = false;
+        userDetails.innerHTML = ``;
+
+    }
+});
